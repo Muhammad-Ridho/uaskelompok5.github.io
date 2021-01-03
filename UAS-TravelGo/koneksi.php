3416d604d0d57fb564ab0bde6c3099ce992dc895
Muhammad-Ridho <56999200+Muhammad-Ridho@users.noreply.github.com>
2021-01-03 23:28:55 +0700

    Add files via upload

---
+++ UAS-TravelGo/koneksi.php
@@ -0,0 +1,10 @@
+<?php
+	$namaHost = "localhost";
+	$username = "root";
+	$password = "";
+	$database = "db_pemesanantravel";
+
+	$connect = mysqli_connect($namaHost, $username, $password, $database);
+
+	
+?>
